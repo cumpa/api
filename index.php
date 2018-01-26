93c493a4fee078d754b74cce08528f078cc5f9b2
cumpa <cupetr@seznam.cz>
2018-01-26 19:41:48 +0100

    fixed .htacces created DB class

--- index.php
+++ index.php
@@ -3,31 +3,30 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
 require 'vendor/autoload.php';
+require 'config.php';
 
 
-$app = new \Slim\App;
+$app = new \Slim\App($config);
+
+$app->get('/', function (Request $request, Response $response, array $args){
+    $message = 'Welcome';
+    return $response->getBody()->write($message);
+});
 
 $app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
     $name = $args['name'];
+    $matches = [];
+    preg_match('/:[a-zA-Z]*/',$name,$matches);
     //$response->getBody()->write("Hello, $name");
     $arr = ['dobre ja' => "Hello, $name"];
-    for ($i='a'; $i < 'z'; $i++) {
-      # code...
-      $arr[$i] = "pismeno - $i";
-
-    }
+    $arr = $matches;
+    //for ($i='a'; $i < 'z'; $i++) {
+    //  # code...
+    //  $arr[$i] = "pismeno - $i";
+//
+    //}
     $response->getBody()->write(json_encode($arr));
     return $response;
 });
 $app->run();
  ?>
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-
-  </body>
-</html>
---
+++ src/db.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Petr
+ * Date: 26.1.2018
+ * Time: 18:57
+ */
+
+
+class DB
+{
+    private $pdo;
+
+    public function __construct(array $dbconfig)
+    {
+        $pdo = new PDO($dbconfig['driver'] . ':host=' . $dbconfig['host'] . ';dbname=' . $dbconfig['dbname'], $dbconfig['user'], $dbconfig['password']);
+    }
+
+    public function select($query, $args)
+    {
+        $matches = [];
+        preg_match(':[a-zA-Z]*',$query,$matches);
+
+        $params = [];
+        $i=0;
+        foreach ($matches as $match){
+            $params[$match] = $args[$i];
+        }
+
+        $stm = $this->pdo->prepare($query);
+        $stm->execute($params);
+
+        return $stm->fetch(PDO::FETCH_ASSOC);
+    }
+}
